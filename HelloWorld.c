e9351bd14d444a30d1edba9f0c890315ce45278e
ojesene1 <darknight4678@gmail.com>
2023-09-27 19:52:03 -0400

    hello.txt

---
+++ HelloWorld.c
@@ -0,0 +1,9 @@
+// importing header files
+# include <stdio.h>
+// the main function
+int main(void){
+    /*calling "printf" function
+    the "definition" is included in "stdio" library*/
+    printf("Hello, McMaster !\n");
+    return 0;
+}
